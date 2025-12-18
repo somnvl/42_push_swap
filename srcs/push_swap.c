bff880f5d005800490151e30a5da9e48d06e9608
somnvl <sophie.menvielle@gmail.com>
2025-12-18 16:53:41 +0100

    pqrsing 0.2, need to add + and - but digits and spaces is working

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: somenvie <somenvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 00:12:34 by somenvie          #+#    #+#             */
-/*   Updated: 2025/12/18 16:51:25 by somenvie         ###   ########.fr       */
+/*   Updated: 2025/12/18 16:53:13 by somenvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	digits_only(int argc, char **argv)
 		{
 			if (ft_isdigit(argv[i][j]) == 0 && (argv[i][j] != ' '))
 				return (1);
+			// manage + and -
 			j++;
 		}
 		i++;
@@ -32,47 +33,15 @@ int	digits_only(int argc, char **argv)
 	return (0);
 }
 
-// if ((argv[i][j] == '+' || argv[i][j] == '-') && ft_isdigit(argv[i][j + 1] == 1))
-			// 	return (1);
-
-// t_list	lst_init(int argc, char **argv)
-// {
-// 	// ft_lstnew
-// 	// split args into nodes
-// }
-
 int	main(int argc, char **argv)
 {
-	/* DIGITS ONLY */
 	if (argc == 1 || digits_only(argc, argv) == 1)
 		ft_printf("Error\n");
 	else
 		ft_printf("OK\n");
 	
-	// lst = lst_init(argc, argv);
-		// generate my list with atoi (which filter min & max)
-	/* < INT_MIN || > INT_MAX */
-	/* DUPLICATA */
+	// generate my lst with a split
+	// atoi each content and filter INT MIN and MAX
+	// check for duplicata
 }
-
-
-// atoi into list args -> filter min && max
-		// check duplicata
-		// algorithm
-
-
-// int	check_duplicata(int nb, t_list *lst)
-// {
-// 	t_list	*tmp;
-
-// 	tmp = lst;
-// 	while (tmp->next)
-// 	{
-// 		if (nb == tmp->content)
-// 			return (1);
-// 		tmp = tmp->next;
-// 	}
-// 	return (0);
-// }
-
 
