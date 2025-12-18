b7e65e5a7141fc68432dc7dc9eab4ef6cf0005f6
somnvl <sophie.menvielle@gmail.com>
2025-12-18 16:47:27 +0100

    ok

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: somenvie <somenvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 00:12:34 by somenvie          #+#    #+#             */
-/*   Updated: 2025/12/18 13:48:09 by somenvie         ###   ########.fr       */
+/*   Updated: 2025/12/18 16:47:05 by somenvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ int	digits_only(int argc, char **argv)
 		j = 0;
 		while (argv[i][j])
 		{
-			if (ft_isdigit(argv[i][j] == 1) 
-				&& argv[i][j] != ' ' && argv[i][j] != '+' && argv[i][j] != '-')
+			if (ft_isdigit(argv[i][j]) == 0 && (argv[i][j] != ' ')) 
 				return (1);
+			// if ((argv[i][j] < '0' || argv[i][j] > '9') 
+			// 	&& (argv[i][j] != ' ') 
+			// 	&& (argv[i][j] != '+' && argv[i][j] != '-'))
+			// 	return (1);
 			j++;
 		}
 		i++;
@@ -33,21 +36,22 @@ int	digits_only(int argc, char **argv)
 	return (0);
 }
 
+// if ((argv[i][j] == '+' || argv[i][j] == '-') && ft_isdigit(argv[i][j + 1] == 1))
+			// 	return (1);
+
 // t_list	lst_init(int argc, char **argv)
 // {
 // 	// ft_lstnew
-// 	// content = atoi de argv
+// 	// split args into nodes
 // }
 
 int	main(int argc, char **argv)
 {
 	/* DIGITS ONLY */
 	if (argc == 1 || digits_only(argc, argv) == 1)
-	{
-		ft_printf("%c\n", argv[0][3]);
 		ft_printf("Error\n");
-		return (0);
-	}
+	else
+		ft_printf("OK\n");
 	
 	// lst = lst_init(argc, argv);
 		// generate my list with atoi (which filter min & max)
