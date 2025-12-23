d07462d1ba8b1440f50baa39b581014566739135
somnvl <sophie.menvielle@gmail.com>
2025-12-23 15:49:22 +0100

    ok

--- srcs/operations.c
+++ srcs/operations.c
@@ -6,7 +6,7 @@
 /*   By: somenvie <somenvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 22:06:50 by somenvie          #+#    #+#             */
-/*   Updated: 2025/12/21 22:17:55 by somenvie         ###   ########.fr       */
+/*   Updated: 2025/12/23 15:49:09 by somenvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,42 @@ void	*pb(t_list *stack_a, t_list *stack_b)
 
 /* (rotate a): Shift up all elements of stack a by 1.
 The first element becomes the last one. */
+void	*ra(t_list *stack_a)
+{
+
+}
 
 /* (rotate b): Shift up all elements of stack b by 1.
 The first element becomes the last one. */
+void	*rb(t_list *stack_b)
+{
+
+}
 
 /* ra and rb at the same time. */
+void	*rr(t_list *stack_a, t_list *stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+}
+
+/* (reverse rotate a): Shift down all elements of stack a by 1.
+The last element becomes the last one. */
+void	*rra(t_list *stack_a)
+{
+
+}
+
+/* (reverse rotate b): Shift down all elements of stack b by 1.
+The first element becomes the last one. */
+void	*rrb(t_list *stack_b)
+{
+
+}
+
+/* rra and rrb at the same time. */
+void	*rrr(t_list *stack_a, t_list *stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+}
